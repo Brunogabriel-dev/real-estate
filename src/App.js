8db324ee7c41d3d6c8abf24a63a7b5913525cee2
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-23 12:40:07 -0300

    Inclusão de texto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 
 // import routes and route
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 // import components
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 // import pages
-import Home from './pages/Home';
-import PropertyDetails from './pages/PropertyDetails';
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
 
 const App = () => {
   return (
-  <div>
+  <div className="max-w-[1440px] mx-auto bg-white">
     <Header />
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/' element={<PropertyDetails />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/property/:id" element=
+      {<PropertyDetails />} />
     </Routes>
     <Footer />
    
