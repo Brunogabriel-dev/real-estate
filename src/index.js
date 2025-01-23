b3f7351458e98da0eadfe42198635f09ea62ecb7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-23 12:06:07 -0300

    Inclusão de texto

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 
 // import router 
-import {} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>
 );
