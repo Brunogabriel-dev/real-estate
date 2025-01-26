91b5c357fb7c0d79083cc3fb08770c362e42bcab
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-26 13:06:34 -0300

    Inclusão de texto

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react"
 
-function Search() {
+// import components 
+import CountryDropdown from "./CountryDropdown";
+import PropertyDropdown from "./PropertyDropdown";
+import PriceRangeDropdown from "./PriceRangeDropdown";
+
+const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+      <CountryDropdown />
+      <PropertyDropdown />
+      <PriceRangeDropdown />
+    </div>
   )
-}
+};
 
-export default Search
+export default Search;
