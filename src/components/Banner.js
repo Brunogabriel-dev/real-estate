919eac8eeadec9357c5de1a1c8969f486d4a0406
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-26 12:58:58 -0300

    Inclusão de texto

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,9 +12,9 @@ const Banner = () => {
       <div className="flex flex-col lg:flex-row">
         <div className="lg: ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
           <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
-            <span>Rent</span>  Your Dream House With Us.
+            <span className="text-violet-700">Rent</span>  Your Dream House With Us.
           </h1>
-          <p>
+          <p className="max-w-[480px] mb-8">
             Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Debitis
             magnam est autem minus expedita accusantium
@@ -23,7 +23,7 @@ const Banner = () => {
           </p>
         </div>
         {/* image */}
-        <div>
+        <div className="hidden flex-1 lg:flex justify-end items-end">
           <img src={Image} alt="" />
         </div>
       </div>
