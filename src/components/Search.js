eeac23136916086bfae1c454482de620070f9546
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-26 13:19:04 -0300

    Inclusão de texto

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,12 +5,18 @@ import CountryDropdown from "./CountryDropdown";
 import PropertyDropdown from "./PropertyDropdown";
 import PriceRangeDropdown from "./PriceRangeDropdown";
 
+// import icons
+import { RiSearch2Line } from  "react-icons/ri"
+
 const Search = () => {
   return (
-    <div>
+    <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between">
       <CountryDropdown />
       <PropertyDropdown />
       <PriceRangeDropdown />
+      <button>
+        <RiSearch2Line />
+      </button>
     </div>
   )
 };
