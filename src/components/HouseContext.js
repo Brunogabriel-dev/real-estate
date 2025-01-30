7b3a6acb384d00f9116ab55f423a6d322b9d8af7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-30 13:40:33 -0300

    Inclusão de texto

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState, createContext } from "react";
 
+// import data
+import {houseData} from "../data"
+
 
 const HouseContext = () => {
   return (
     <div>HouseContext</div>
   )
-}
+};
 
-export default HouseContext
+export default HouseContext;
