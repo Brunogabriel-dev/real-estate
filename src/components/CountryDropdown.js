fbb226da6f5acf2fed78b418c6f05f4f915475fe
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-02-04 10:26:45 -0300

    Inclusão de texto

--- src/components/CountryDropdown.js
+++ src/components/CountryDropdown.js
@@ -1,12 +1,19 @@
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, useContext } from "react";
 
 // import icons
 import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri'
 
+// import headless ui
+import { Menu } from "@headlessui/react";
+
+// import house context
+import { HouseContext } from "./HouseContext";
+
 const CountryDropdown = () => {
+  const {country, setCountry, countries} = useContext(HouseContext);
   return (
     <div>CountryDropdown</div>
-  )
-}
+  );
+};
 
-export default CountryDropdown
+export default CountryDropdown;
